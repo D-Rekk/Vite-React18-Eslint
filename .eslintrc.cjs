35eab4fc4d1ea6d79707e860d521661003fcf9b5
D-Rek <andrea_derek@hotmail.it>
2023-03-20 20:23:19 +0100

    Added absolute path behaviour - Configured tsconfig.json to utilize absolute path config - Changed eslintrc to recognize absolute path - Added vite-tsconfig-paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,8 +15,10 @@ module.exports = {
     "import/resolver": {
       node: {
         paths: ["src"],
+        moduleDirectory: ["node_modules", "src/"],
         extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
     },
     typescript: {
       alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
@@ -32,6 +34,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
+    "plugin:import/typescript",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended", // Make sure this is always the last element in the array.
   ],
